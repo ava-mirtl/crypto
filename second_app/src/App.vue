c0d23374909a9be016a70cf87b4867e7ed5fb270
ava-mirtl <ava_mirtl@mail.ru>
2024-02-03 17:54:58 +0300

    second commit

--- second_app/src/App.vue
+++ second_app/src/App.vue
@@ -1,15 +1,71 @@
-<script setup>
-
+<script >
+import Input from './components/Input.vue';
+import Selector from './components/Selector.vue';
+export default {
+components: {
+  Input, Selector
+},
+data(){
+  return {
+amount: 0,
+error: '',
+cryptoFirst: '',
+cryptoSecond: '',  }
+},
+methods:{
+  changeAmount(val){
+    this.amount = val
+  },
+  setCryptoFirst(val){
+    this.cryptoFirst = val
+  },
+  setCryptoSecond(val){
+    this.cryptoSecond = val
+  },
+  convert(){
+    if(this.amount <=0){
+      this.error="Введите число больше ноля";
+      return;
+    }
+    else if(this.cryptoFirst == ''|| this.cryptoSecond == ''){
+      this.error="Выберите валюту";
+      return;
+    }
+    else if(this.cryptoFirst == this.cryptoSecond){
+      this.error="Выберите другую валюту";
+      return;
+    }
+   
+    this.error = '';
+  }
+}
+}
 </script>
 
 <template>
 <h1>CRYPTO</h1>
-<input type="number">
+<Input :changeAmount="changeAmount" :convert="convert"/>
+<p v-if="error!=''" class="error">{{ error }}</p>
+<div className="selectors">
+<Selector :setCrypto="setCryptoFirst"/>
+<Selector :setCrypto="setCryptoSecond"/>
+</div>
+
 </template>
 
 <style scoped>
 h1{
   font-family: 'Nabla', system-ui;
   font-size: 10em;
+}
+.selectors{
+  display: flex;
+  justify-content: space-around;
+  width: 650px;
+  margin: 0 auto;
+}
+.error{
+  color: brown;
+  margin-bottom: 20px;
 }
 </style>
--- second_app/src/components/Input.vue
+++ second_app/src/components/Input.vue
@@ -1,13 +1,44 @@
 <script>
 export default{
-  
+props:{
+  changeAmount:{
+    type: Function,
+    required: true
+  },
+  convert:{
+    type: Function,
+    required: true
+  }
+}
 }
 </script>
 
 <template>
-<input type="number">
+<input min='0' type="number" @input="changeAmount($event.target.value)" placeholder="Введите число">
+<br/>
+<button @click="convert()">Конвертировать</button>
 </template>
 
 <style scoped>
-
+input{
+  outline: none;
+  border-radius: 3px;
+  width: 500px;
+  font-size: 1.2em;
+  border: 0;
+  padding: 10px 15px;
+  background: #fafafa;
+  color: #333;
+  margin-bottom: 20px;
+}
+button{
+  padding: 15px 20px;
+  color: #fff;
+  text-transform: uppercase;
+  cursor: pointer;
+  background: #1a032d;
+  border: 0;
+  border-radius: 3px;
+  margin-bottom: 20px;
+}
 </style>
