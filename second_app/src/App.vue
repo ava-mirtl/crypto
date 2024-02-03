987899a9da206b3602a5edeb0ea6885f53cacb97
ava-mirtl <ava_mirtl@mail.ru>
2024-02-03 16:55:55 +0300

    first commit

---
+++ second_app/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+<h1>CRYPTO</h1>
+<input type="number">
+</template>
+
+<style scoped>
+h1{
+  font-family: 'Nabla', system-ui;
+  font-size: 10em;
+}
+</style>
---
+++ second_app/src/components/Input.vue
@@ -0,0 +1,13 @@
+<script>
+export default{
+  
+}
+</script>
+
+<template>
+<input type="number">
+</template>
+
+<style scoped>
+
+</style>
